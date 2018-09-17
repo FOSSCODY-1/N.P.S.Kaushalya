f2c62fb78cb8ffb0d30280a5def2813aa963f251
SupuniNuwarapaksha <supuninuwarapaksha@gmail.com>
2018-09-17 22:23:29 +0530

    1stcommit

---
+++ start.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() { char op[3];
+    printf("Are you a customer(yes/no)\n");
+    scanf("%s",&op);
+    if(op[0]=='n'){
+            char psswd;
+            printf("Enter your password\n");
+            scanf("%s",&psswd);
+            if(psswd=="ucsc@123") {
+
+            }
+
+
+    }
+    else {
+
+    }
+}
